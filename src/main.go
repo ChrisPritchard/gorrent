8d5c4936bb8a230328e1b8da2c57932f6a4cd43c
Chris <chris@grislygrotto.nz>
2026-01-02 22:08:01 +1300

    work on calling tracker

--- src/main.go
+++ src/main.go
@@ -7,13 +7,33 @@ import (
 
 func main() {
 	file := "c:\\users\\chris\\onedrive\\desktop\\test.torrent"
-	d, _ := os.ReadFile(file)
-	res, rem, err := decode_from_bencoded(d)
-	fmt.Println(res)
-	fmt.Println(rem)
-	fmt.Println(err)
+
+	err := try_download(file)
+	if err != nil {
+		fmt.Printf("unable to download from torrent file: %v", err)
+		os.Exit(1)
+	}
+}
+
+func try_download(torrent_file_path string) error {
+	d, err := os.ReadFile(torrent_file_path)
+	if err != nil {
+		return fmt.Errorf("unable to read file at path %s: %v", torrent_file_path, err)
+	}
 
 	torrent, err := parse_torrent_file(d)
-	fmt.Println(torrent)
-	fmt.Println(err)
+	if err != nil {
+		return fmt.Errorf("unable to parse torrent file: %v", err)
+	}
+
+	peer_info, err := call_tracker(torrent)
+	if err != nil {
+		return fmt.Errorf("failed to register with tracker: %v", err)
+	}
+
+	fmt.Println(peer_info)
+	// handshake with peers
+	// download and write file
+
+	return nil
 }
