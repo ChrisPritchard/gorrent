e795789779070e16344fe839a45d4a8a314dcbfa
Chris <chris@grislygrotto.nz>
2026-01-10 14:30:53 +1300

    wrote getter with tests

--- internal/peer/bitfield.go
+++ internal/peer/bitfield.go
@@ -17,3 +17,15 @@ func (bf *BitField) Set(index uint) error {
 	bf.data[b] |= byte(n)
 	return nil
 }
+
+func (bf *BitField) Get(index uint) (bool, error) {
+	b := index / 8
+	if b >= uint(len(bf.data)) {
+		return false, fmt.Errorf("index is out of range of valid bitfield values")
+	}
+	m := index % 8
+	n := 1
+	n = n << m
+	res := bf.data[b] & byte(n)
+	return res != 0, nil
+}
--- internal/peer/bitfield_test.go
+++ internal/peer/bitfield_test.go
@@ -79,3 +79,86 @@ func TestBitfieldSet(t *testing.T) {
 		})
 	}
 }
+
+func TestBitfieldGet(t *testing.T) {
+	tests := []struct {
+		name     string
+		state    []byte
+		index    uint
+		want     bool
+		want_err bool
+	}{
+		{
+			name:     "single byte get true",
+			state:    []byte{0b00010000},
+			index:    4,
+			want:     true,
+			want_err: false,
+		},
+
+		{
+			name:     "single byte get false",
+			state:    []byte{0},
+			index:    4,
+			want:     false,
+			want_err: false,
+		},
+
+		{
+			name:     "single byte invalid 1",
+			state:    []byte{0},
+			index:    8,
+			want:     false,
+			want_err: true,
+		},
+
+		{
+			name:     "single byte invalid 2",
+			state:    []byte{0},
+			index:    453,
+			want:     false,
+			want_err: true,
+		},
+
+		{
+			name:     "multi byte get true",
+			state:    []byte{0, 0b00010000, 0},
+			index:    12,
+			want:     true,
+			want_err: false,
+		},
+
+		{
+			name:     "multi byte get false",
+			state:    []byte{0, 0, 0},
+			index:    12,
+			want:     false,
+			want_err: false,
+		},
+
+		{
+			name:     "multi byte invalid",
+			state:    []byte{0, 0, 0},
+			index:    153,
+			want:     false,
+			want_err: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			bf := BitField{data: tt.state}
+
+			r, err := bf.Get(tt.index)
+			if (err != nil) != tt.want_err {
+				t.Errorf("parse() error = %v, wantErr %v", err, tt.want_err)
+				return
+			}
+
+			if err == nil && r != tt.want {
+				t.Errorf("parse() = %v, want %v", bf.data, tt.want)
+			}
+		})
+	}
+}
